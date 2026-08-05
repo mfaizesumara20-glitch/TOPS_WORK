0cb33018e54360ae1d345bd596f07aaf7a0433d9
Mohammad faize sumra <mfaizesumara20@gmail.com>
2026-08-05 13:36:08 +0530

    05-08-2026

---
+++ EXTRA/11-practice.py
@@ -0,0 +1,82 @@
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i+1
+
+# print('Done')
+
+
+# num = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+# while True:
+#     if num%2==0:
+#         print(num)
+#     else:
+#         break
+
+
+# num = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+# i = 1
+# while i <= 20:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+
+
+
+# i = 20
+
+# while i >= 1:
+#     print(i)
+#     i = i-1
+
+
+
+# i = 1
+# while i <= 10:
+#     res = 7 * i
+#     print(7 , 'x',i, '=', res)
+#     i = i + 1
+
+
+
+# total = 0
+# i = 1
+# while i <= 10:
+#     total = total + i
+#     i = i + 1
+
+
+# print(total)
+
+
+
+
+
+# count = 0
+# i = 1
+# while i <= 20:
+#     if i % 2 == 0:
+#         print(i)
+#         count += 1
+#     i += 1
+
+
+# print('Total even numbers:', count)
+
+
+
+numbers = [12, 45, 7, 89, 23, 67]
+
+largest = numbers[0]
+i = 0
+
+while i < len(numbers):
+    if numbers[i] > largest:
+        largest = numbers[i]
+    i += 1
+
+print("Largest number:", largest) 
+
+
+
